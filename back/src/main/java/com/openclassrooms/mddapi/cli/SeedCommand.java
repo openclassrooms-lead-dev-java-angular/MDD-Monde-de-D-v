38843b94b7b556beffea76292910955d17681f18
Frederic Mas <fredfredfredm@yahoo.fr>
2026-09-11 09:26:38 +0200

    chore(setup/backend-configuration): add cli and seed command for seed data in database, and update readme with rsa pair keys generation

---
+++ back/src/main/java/com/openclassrooms/mddapi/cli/SeedCommand.java
@@ -0,0 +1,33 @@
+package com.openclassrooms.mddapi.cli;
+
+import com.openclassrooms.mddapi.seeder.UserSeeder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import picocli.CommandLine.Command;
+
+@Log4j2
+@Component
+@Command(
+        name = "seed",
+        description = "Generate application seed data"
+)
+@RequiredArgsConstructor
+public class SeedCommand implements ApplicationRunner {
+
+    private final UserSeeder userSeeder;
+
+    @Override
+    public void run(ApplicationArguments args) {
+
+        if (!args.containsOption("seed")) {
+            return;
+        }
+
+        log.info("Seed command start");
+
+        log.info("Seed command finished");
+    }
+}
