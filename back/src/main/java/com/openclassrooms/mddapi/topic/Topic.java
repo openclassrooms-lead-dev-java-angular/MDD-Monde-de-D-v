2b73c4527a41e92fa0f22be5baca585b58ff9b47
Frederic Mas <fredfredfredm@yahoo.fr>
2026-09-16 14:20:00 +0200

    feat(feature/topics): add topic entity

---
+++ back/src/main/java/com/openclassrooms/mddapi/topic/Topic.java
@@ -0,0 +1,41 @@
+package com.openclassrooms.mddapi.topic;
+
+import com.openclassrooms.mddapi.common.entity.BaseEntity;
+import jakarta.annotation.Resource;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "topics")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id"}, callSuper = false)
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Topic extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            nullable = false,
+            length = 50
+    )
+    private String name;
+
+    @Column(
+            nullable = false,
+            unique = true,
+            length = 90
+    )
+    private String slug;
+
+    @Column(length = 255)
+    private String description;
+}
