7e5f2b77244b1f3588701d6a006d9abcf3901229
Frederic Mas <fredfredfredm@yahoo.fr>
2026-09-16 14:24:39 +0200

    feat(feature/topics): add TopicSeeder

---
+++ back/src/main/java/com/openclassrooms/mddapi/seeder/TopicSeeder.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.mddapi.seeder;
+
+import com.openclassrooms.mddapi.topic.Topic;
+import com.openclassrooms.mddapi.topic.TopicRepository;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Log4j2
+@Component
+public class TopicSeeder extends AbstractSeeder<Topic> {
+
+    public TopicSeeder(TopicRepository topicRepository) {
+        super(topicRepository);
+        log.info("TopicSeeder repository: {}", topicRepository);
+    }
+
+    @Override
+    protected List<Topic> getEntities() {
+        List<String> languages = List.of(
+                "Java", "TypeScript", "JavaScript", "Cobol", "Python"
+        );
+
+        return languages.stream()
+                .map(language -> {
+                    Topic topic = new Topic();
+                    topic.setName(language);
+                    topic.setSlug(language);
+                    topic.setDescription(language + " development.");
+                    return topic;
+                })
+                .toList();
+    }
+}
